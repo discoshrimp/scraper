e649259a1274d04c195ba7819e2489284f3478f6
discoshrimp <roydega@gmail.com>
2018-11-02 15:45:36 -0700

    still fiddling

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static('public'))
 
 //connect to MongoDB
 mongoose.Promise = Promise;
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/scrape")
+mongoose.connect(process.env.MONGODB_URI)
 
 
 //routes
